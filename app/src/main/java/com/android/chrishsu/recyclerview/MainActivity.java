664f61a012f94d3fcb5863cbf6ccca1635a75da4
yaostyle <yaostyle@gmail.com>
2018-05-22 17:38:01 -0700

    Initial commit

---
+++ app/src/main/java/com/android/chrishsu/recyclerview/MainActivity.java
@@ -0,0 +1,65 @@
+package com.android.chrishsu.recyclerview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity {
+
+    RecyclerView recyclerView;
+    ProductAdapter adapter;
+    List<Product> productList;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        productList = new ArrayList<>();
+
+        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+        recyclerView.setHasFixedSize(true);
+
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        productList = new ArrayList<>();
+
+
+        //adding some items to our list
+        productList.add(
+                new Product(
+                        1,
+                        "Apple MacBook Air Core i5 5th Gen - (8 GB/128 GB SSD/Mac OS Sierra)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.macbook));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Dell Inspiron 7000 Core i5 7th Gen - (8 GB/1 TB HDD/Windows 10 Home)",
+                        "14 inch, Gray, 1.659 kg",
+                        4.3,
+                        60000,
+                        R.drawable.dellinspiron));
+
+        productList.add(
+                new Product(
+                        1,
+                        "Microsoft Surface Pro 4 Core m3 6th Gen - (4 GB/128 GB SSD/Windows 10)",
+                        "13.3 inch, Silver, 1.35 kg",
+                        4.3,
+                        60000,
+                        R.drawable.surface));
+
+        adapter = new ProductAdapter(this, productList);
+
+        recyclerView.setAdapter(adapter);
+    }
+}
